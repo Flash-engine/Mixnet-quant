e3726fe5877898ea52cef594adcd032b1d2641dc
liuxin <liuxin@sensetime.com>
2019-10-12 09:54:53 +0800

    resolve conflicts

---
+++ FastAutoAugment/data.py
@@ -0,0 +1,159 @@
+import logging
+import os
+
+import torch
+import torchvision
+
+from torch.utils.data import SubsetRandomSampler, Sampler, Subset
+from torchvision.transforms import transforms
+from sklearn.model_selection import StratifiedShuffleSplit
+from theconf import Config as C
+
+from archive import arsaug_policy, autoaug_policy, autoaug_paper_cifar10, random_search2048, \
+    fa_reduced_imagenet, fa_reduced_cifar10
+# fa_wresnet40x2, fa_wresnet28x10, fa_pyramid_e300, fa_pyramid_c100, fa_wresnet40x2_c100, \
+# fa_resnet50_imagenet_minusloss, fa_wresnet40x2_cifar100, fa_wresnet40x2_cifar100_r5, fa_wresnet40x2_cifar10, \
+# fa_wresnet28x10_cifar100, fa_wresnet28x10_cifar10, fa_pyramid_cifar10, fa_pyramid_cifar100, \
+# fa_resnet50_rimagenet, fa_shake26_2x96d_cifar100, fa_shake26_2x96d_cifar10, fa_reduced_cifar10_progressive
+from augmentations import *
+from common import get_logger
+from samplers.distributed_sampler import DistributedStratifiedSampler
+from samplers.stratified_sampler import StratifiedSampler
+
+logger = get_logger('MicroNet')
+logger.setLevel(logging.INFO)
+
+def get_dataloaders(dataset, batch, dataroot, split=0.0, split_idx=0, horovod=False):
+    if 'cifar' in dataset:
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    else:
+        raise ValueError('dataset=%s' % dataset)
+
+    if C.get()['cutout'] > 0:
+        logger.debug('cutout provided.')
+        transform_train.transforms.append(CutoutDefault(C.get()['cutout']))
+
+    if dataset == 'cifar100':
+        total_trainset = torchvision.datasets.CIFAR100(root=dataroot, train=True, download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR100(root=dataroot, train=False, download=True, transform=transform_test)
+    else:
+        raise ValueError('invalid dataset name=%s' % dataset)
+
+    if split > 0.0:
+        sss = StratifiedShuffleSplit(n_splits=5, test_size=split, random_state=0)
+        sss = sss.split(list(range(len(total_trainset))), total_trainset.train_labels)
+        for _ in range(split_idx + 1):
+            train_idx, valid_idx = next(sss)
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetSampler(valid_idx)
+
+        if horovod:
+            import horovod.torch as hvd
+            train_sampler = torch.utils.data.distributed.DistributedSampler(train_sampler, num_replicas=hvd.size(), rank=hvd.rank())
+    else:
+        valid_sampler = SubsetSampler([])
+
+        if horovod:
+            import horovod.torch as hvd
+            train_sampler = DistributedStratifiedSampler(total_trainset.train_labels, num_replicas=hvd.size(), rank=hvd.rank())
+        else:
+            train_sampler = StratifiedSampler(total_trainset.train_labels)
+
+    trainloader = torch.utils.data.DataLoader(
+        total_trainset, batch_size=batch, shuffle=True if train_sampler is None else False, num_workers=32, pin_memory=True,
+        sampler=train_sampler, drop_last=True)
+    validloader = torch.utils.data.DataLoader(
+        total_trainset, batch_size=batch, shuffle=False, num_workers=16, pin_memory=True,
+        sampler=valid_sampler, drop_last=False)
+
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch, shuffle=False, num_workers=32, pin_memory=True,
+        drop_last=False
+    )
+    return train_sampler, trainloader, validloader, testloader
+
+
+class CutoutDefault(object):
+    """
+    Reference : https://github.com/quark0/darts/blob/master/cnn/utils.py
+    """
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, img):
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
+
+
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
+
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = eigval
+        self.eigvec = eigvec
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone()\
+            .mul(alpha.view(1, 3).expand(3, 3))\
+            .mul(self.eigval.view(1, 3).expand(3, 3))\
+            .sum(1).squeeze()
+
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
+
+class Augmentation(object):
+    def __init__(self, policies):
+        self.policies = policies
+
+    def __call__(self, img):
+        for _ in range(1):
+            policy = random.choice(self.policies)
+            for name, pr, level in policy:
+                if random.random() > pr:
+                    continue
+                img = apply_augment(img, name, level)
+        return img
+
+
+class SubsetSampler(Sampler):
+    r"""Samples elements from a given list of indices, without replacement.
+
+    Arguments:
+        indices (sequence): a sequence of indices
+    """
+
+    def __init__(self, indices):
+        self.indices = indices
+
+    def __iter__(self):
+        return (i for i in self.indices)
+
+    def __len__(self):
+        return len(self.indices)
